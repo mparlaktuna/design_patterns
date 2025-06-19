5dc5ff33625e7744caeef170225f16fd2645d401
mparlaktuna <mparlaktuna@users.noreply.github.com>
2025-06-19 22:50:07 +0000

    deploy: 3a9e03820350619c9b6ff79aa1e5c7cd53fc82a7

---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['design_20patterns_0',['Design Patterns',['../md_documentation_2main.html',1,'']]]
+];
---
+++ search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['patterns_0',['Design Patterns',['../md_documentation_2main.html',1,'']]]
+];
